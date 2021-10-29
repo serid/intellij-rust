fb0451076b0bfd6cc563d305ebd18b51f0de978d
Mikhail Chernyavsky <mikhail.chernyavsky@jetbrains.com>
2021-10-29 14:20:03 +0300

    UI: Move settings for linters to another configurable

--- src/main/kotlin/org/rust/cargo/project/configurable/CargoConfigurable.kt
+++ src/main/kotlin/org/rust/cargo/project/configurable/CargoConfigurable.kt
@@ -7,13 +7,7 @@ package org.rust.cargo.project.configurable
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
-import com.intellij.ui.EnumComboBoxModel
-import com.intellij.ui.components.JBCheckBox
-import com.intellij.ui.layout.*
-import org.rust.cargo.project.model.cargoProjects
-import org.rust.cargo.toolchain.ExternalLinter
-import org.rust.cargo.util.CargoCommandCompletionProvider
-import org.rust.cargo.util.RsCommandLineEditor
+import com.intellij.ui.layout.panel
 
 class CargoConfigurable(project: Project) : RsConfigurableBase(project, "Cargo") {
     override fun createPanel(): DialogPanel = panel {
@@ -42,47 +36,6 @@ class CargoConfigurable(project: Project) : RsConfigurableBase(project, "Cargo")
                 state::useOffline,
                 comment = "Pass <b>--offline</b> option to Cargo not to perform network requests"
             )
-        }
-
-        titledRow("External Linter") {
-            subRowIndent = 0
-            row("External tool:") {
-                comboBox(
-                    EnumComboBoxModel(ExternalLinter::class.java),
-                    state::externalLinter,
-                ).comment("External tool to use for code analysis")
-            }
-
-            row("Additional arguments:") {
-                RsCommandLineEditor(project, CargoCommandCompletionProvider(project.cargoProjects, "check ") { null })(CCFlags.growX)
-                    .comment("Additional arguments to pass to <b>cargo check</b> or <b>cargo clippy</b> command")
-                    .withBinding(
-                        componentGet = { it.text },
-                        componentSet = { component, value -> component.text = value },
-                        modelBinding = state::externalLinterArguments.toBinding()
-                    )
-            }
-
-            lateinit var runExternalLinterOnTheFly: CellBuilder<JBCheckBox>
-            row {
-                runExternalLinterOnTheFly = checkBox(
-                    "Run external linter to analyze code on the fly",
-                    state::runExternalLinterOnTheFly,
-                    comment = """
-                        Enable external linter to add code highlighting based on the used linter result.
-                        Can be CPU-consuming
-                    """.trimIndent()
-                )
-            }
-
-            row {
-                cell {
-                    intTextField(state::externalLinterOnTheFlyMaxDuration, columns = 4, range = 1..9999)
-                        .enableIf(runExternalLinterOnTheFly.selected)
-                    label("Max duration of external linter run (ms)")
-                        .comment("Show notification warning if the external linter is running longer than the set value")
-                }
-            }
         }
     }
 }
